989e737afd9ead7f0535f2ce69b2128fb3c1161a
Sofia Huts <sofia.huts@gmail.com>
2018-03-17 10:05:30 +0200

    Add skeleton

--- event-on-fly/src/app/app.module.ts
+++ event-on-fly/src/app/app.module.ts
@@ -1,16 +1,32 @@
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyDFey1JYAWe874FyVKIZ5b-IzqZjm9zBSU",
+  authDomain: "event-on-fly.firebaseapp.com",
+  databaseURL: "https://event-on-fly.firebaseio.com",
+  projectId: "event-on-fly"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
