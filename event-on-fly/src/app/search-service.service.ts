d42bd9400ceebcea130135e9ae6d203205e2528b
Marta <marta.maxymiak@gmail.com>
2018-03-17 22:42:07 +0200

    filter

--- event-on-fly/src/app/search-service.service.ts
+++ event-on-fly/src/app/search-service.service.ts
@@ -17,37 +17,39 @@ export class SearchServiceService {
   public currentPage: CurrentPage = CurrentPage.StartFiler;
 
   venues: Venue[];
-  
+
   constructor(private db: AngularFireDatabase) {
   }
 
   filteredVenues: any;
 
-  requestedVenue: VenueFilter;
+  requestedVenue: AdditionalServices;
 
   getBundleList(): Venue[] {
-   var subscription = this.db.list<Venue>("/venues").valueChanges().subscribe(venueData => {
-    this.venues = venueData;
-   this.bundles = this.getFuckingBundles(this.venues);
-  });
-  
-    var getService= this.searchRequest.additionalServices.find(service => service.type == ServiceType.Venue);
-    this.requestedVenue = <VenueFilter>(this.searchRequest.additionalServices.find(service => service.type == ServiceType.Venue).service);
+    var subscription = this.db.list<Venue>("/venues").valueChanges().subscribe(venueData => {
+      this.venues = venueData;
+      this.requestedVenue = this.searchRequest.additionalServices.find(service => service.type == ServiceType.Venue);
+      this.bundles = this.getFuckingBundles(this.venues);
+    });
 
     return this.venues;
   }
 
   private getFuckingBundles(venuesData) {
-     var capacityMatched = this.filterByCapacity();
+    var capacityMatched = this.filteredVenue(); //this.filterByCapacity();
 
-     var bundleData = [];
+    var bundleData = [];
 
-     var bundle = new Bundle();
-     bundle.price = 1000;
-     bundle.venue = capacityMatched[0];
+    bundleData.push(this.newBundle(capacityMatched));
+    return bundleData;
+  }
 
-     bundleData.push(bundle);
-     return bundleData;
+  private newBundle(capacityMatched) {
+    var bundle = new Bundle();
+    bundle.price = 1000;
+    bundle.venue = capacityMatched[0];
+
+    return bundle;
   }
 
   private filterByCapacity() {
@@ -55,18 +57,42 @@ export class SearchServiceService {
     return this.venues.filter(venue => venue.peopleNumber >= requestedNumber);
   }
 
-  private filterByPrice() {
-    var priceFrom = this.requestedVenue.priceFrom;
-    var priceTo = this.requestedVenue.priceTo;
+  // private filterByPrice() {
+  //  var reqVenue =  <VenueFilter>(this.requestedVenue);
+  //   var priceFrom = this.requestedVenue.priceFrom;
+  //   var priceTo = this.requestedVenue.priceTo;
 
-    return this.venues.filter(venue => venue.price.amount < priceTo && venue.price.amount > priceFrom);
-  }
+  //   return this.venues.filter(venue => venue.price.amount < priceTo && venue.price.amount > priceFrom);
+  // }
 
-  private filterBySquareSize() {
-    var squareFrom = this.requestedVenue.squareFrom;
-    var squareTo = this.requestedVenue.squareTo;
+  // private filterBySquareSize() {
+  //   var squareFrom = this.requestedVenue.squareFrom;
+  //   var squareTo = this.requestedVenue.squareTo;
 
-    return this.venues.filter(venue => venue.square < squareFrom && venue.square > squareTo);
+  //   return this.venues.filter(venue => venue.square < squareFrom && venue.square > squareTo);
+  // }
+
+  private filteredVenue() {
+    var reqVenue =  <VenueFilter>(this.requestedVenue.service);
+    var requestedNumber = 0;
+    var squareFrom = 0; 
+    var squareTo = 0; 
+
+    if (this.searchRequest.simpleFilter.peopleNumber) {
+      requestedNumber = this.searchRequest.simpleFilter.peopleNumber;
+    }
+
+    if(reqVenue.squareFrom) {
+      squareFrom = reqVenue.squareFrom;
+    }
+
+    if(reqVenue.squareTo) {
+      squareTo = reqVenue.squareTo;
+    }
+
+    return this.venues.filter(venue => venue.peopleNumber >= requestedNumber
+                               && venue.square < squareFrom
+                                && venue.square > squareTo);
   }
 }
 
