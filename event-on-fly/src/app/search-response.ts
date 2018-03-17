45022e94ed90318bbdb39d9ec88ad3ae8711b193
Marta Maxymiak <marta.maxymiak@gmail.com>
2018-03-17 11:48:22 +0200

    init base request data

---
+++ event-on-fly/src/app/search-response.ts
@@ -0,0 +1,7 @@
+export class SearchResponse {
+   bundles: Bundles[];
+}
+
+export class Bundles {
+
+}
--- event-on-fly/src/app/search-service.service.ts
+++ event-on-fly/src/app/search-service.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { SearchRequest } from './search-request';
+import { SearchResponse, Bundles } from './search-response';
+import { FirebaseListObservable } from 'angularfire2/database-deprecated';
 
 @Injectable()
 export class SearchServiceService {
 
   private basePath: string = '/bundles';
 
+  bundles: FirebaseListObservable<Bundles[]> = null;
+  
   constructor(private db: AngularFireDatabase) { }
 
+  getBundleList(query={}): FirebaseListObservable<Bundles[]> {
+    // this.bundles = this.db.list(this.basePath, {
+    //    query : query
+    // });
+
+    return this.bundles;
+  }
 }
