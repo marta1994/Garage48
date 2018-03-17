4c69a03f4f989c0481bdd3a29a4d1e3dd78a67d6
Marta Maxymiak <marta.maxymiak@gmail.com>
2018-03-17 15:03:02 +0200

    filter data by basic condition

--- event-on-fly/src/app/search-service.service.ts
+++ event-on-fly/src/app/search-service.service.ts
@@ -1,26 +1,82 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
-import { SearchRequest } from './search-request';
-import { SearchResponse, Bundles } from './search-response';
+import { SearchRequest, VenueFilter, IService, AdditionalServices } from './search-request';
+import { SearchResponse, Venue } from './search-response';
 import { FirebaseListObservable } from 'angularfire2/database-deprecated';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class SearchServiceService {
 
-  private basePath: string = '/bundles';
-
-  bundles: FirebaseListObservable<Bundles[]> = null;
+  private basePath: string = '/venues';
 
   public searchRequest: SearchRequest = new SearchRequest();
 
-  constructor(private db: AngularFireDatabase) { 
+  venues: Observable<Venue[]> = null;
+
+  constructor(private db: AngularFireDatabase) {
   }
 
-  getBundleList(query={}): FirebaseListObservable<Bundles[]> {
-    // this.bundles = this.db.list(this.basePath, {
-    //    query : query
-    // });
+  /// Active filter rules
+  filters = {}
 
-    return this.bundles;
+  filteredVenues: any;
+
+  requestedVenue: VenueFilter[];
+
+  getBundleList(): Observable<Venue[]> {
+    this.venues = this.db.list<Venue>(this.basePath).valueChanges();
+
+    //  this.requestedVenue = this.searchRequest.additionalServices.filter(s => <VenueFilter>s.service, null);
+     
+
+    return this.venues;
+  }
+
+  private filterByCapacity(){
+    var requestedNumber = this.searchRequest.simpleFilter.peopleNumber;
+    this.filteredVenues = this.venues.map(venue => venue.filter(v => v.peopleNumber == requestedNumber));
+  }
+
+  private filterByPrice(){
+    var priceRange = this.searchRequest.simpleFilter.priceRange;
+    this.filteredVenues = this.venues.map(venue => venue.filter(v => v.price < && v.price > ));
+  }
+
+  private filterBySquareSize(){
+    this.searchRequest.additionalServices.filter(s => (<VenueFilter>s.service).squareRange);
+    
+  }
+
+  private applyFilters() {
+    
+  }
+
+   /// filter property by equality to rule
+   filterExact(property: string, rule: any) {
+    this.filters[property] = val => val == rule
+    this.applyFilters()
+  }
+
+  /// filter  numbers greater than rule
+  filterGreaterThan(property: string, rule: number) {
+    this.filters[property] = val => val > rule
+    this.applyFilters()
+  }
+
+  /// filter properties that resolve to true
+  filterBoolean(property: string, rule: boolean) {
+    if (!rule) this.removeFilter(property)
+    else {
+      this.filters[property] = val => val
+      this.applyFilters()
+    }
+  }
+
+  /// removes filter
+  removeFilter(property: string) {
+    delete this.filters[property]
+    this[property] = null
+    this.applyFilters()
   }
 }
