87bd3952cbf1390eb040885322569c3f4542f25d
marta1994 <mmmz1004@gmail.com>
2018-03-18 16:35:48 +0200

    dsdfsd

--- event-on-fly/src/app/start/start.component.ts
+++ event-on-fly/src/app/start/start.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchServiceService } from '../search-service.service';
 import { LoadingService } from '../loading.service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-start',
@@ -9,9 +10,16 @@ import { LoadingService } from '../loading.service';
 })
 export class StartComponent implements OnInit {
 
-  constructor(public loadingService: LoadingService, public searchService: SearchServiceService) { }
+  constructor(public loadingService: LoadingService, public searchService: SearchServiceService,
+  private router: Router) { }
 
   ngOnInit() {
+  }
+
+  public search() {
+    this.loadingService.startLoading();
+    this.searchService.getBundleList();
+    this.router.navigate(["search"], { skipLocationChange: false });
   }
 
 }
